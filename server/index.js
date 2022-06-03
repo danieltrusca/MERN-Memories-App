d8c1e83643719b0582dfe2583e89a2ef26b39e82
danieltrusca <truscadani@yahoo.com>
2022-06-03 14:46:26 +0300

    better

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const path = require("path");
 
 // load env variables
 const dotenv = require("dotenv");
@@ -17,11 +16,14 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // routes
 app.use("/api/posts", postRotes);
 app.use("/api/auth", userRoutes);
+
+app.get("/", (req, res)=>{
+  res.send('APP is RUNNING');
+})
 
 const PORT = process.env.PORT || 5000;
 
@@ -30,12 +32,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-      app.listen(PORT, () =>
+  .then(() =>  app.listen(PORT, () =>
         console.log(`Server Running on Port: http://localhost:${PORT}`)
-       );
-    });
-  })
+      ))
   .catch((error) => console.log(`${error} did not connect`));
