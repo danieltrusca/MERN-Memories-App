8e5733c4c4d5215b61ba3aae4d01a85b948d0e37
danieltrusca <truscadani@yahoo.com>
2022-06-03 13:43:15 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // load env variables
 const dotenv = require("dotenv");
 dotenv.config();
 
 // import routes
-const postRotes=require("./routes/posts");
-const userRoutes=require("./routes/user");
+const postRotes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const app = express();
-
-
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // routes
 app.use("/api/posts", postRotes);
@@ -26,12 +26,16 @@ app.use("/api/auth", userRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+      app.listen(PORT, () =>
+        console.log(`Server Running on Port: http://localhost:${PORT}`)
+       );
+    });
+  })
   .catch((error) => console.log(`${error} did not connect`));
-
-
