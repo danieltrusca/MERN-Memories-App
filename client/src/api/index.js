dda3f0d50995118416e6da00746a6cf0cdd44521
danieltrusca <truscadani@yahoo.com>
2022-06-03 15:07:10 +0300

    preparing for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+// const API = axios.create({ baseURL: "http://localhost:5000/api" });
+
+const API = axios.create({ baseURL: "https://mern-memories-dan.herokuapp.com/api" });
+
+
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
